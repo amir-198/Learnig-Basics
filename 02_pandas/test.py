f599051a05ed467a916e688bcd8e4d1a6982e58a
amir-198 <engramir1992@gmail.com>
2022-07-24 19:38:47 +0500

    pandas dataset upload

--- 02_pandas/test.py
+++ 02_pandas/test.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.load_dataset('tips')
-
+tips = sns.load_dataset('tips')
+sns.lineplot(x='day', y='total_bill', data=tips)
 
 
